b0386f6aa84aa82e407b0240d1f8d894af3025b2
ottojolanki <ojolanki@stanford.edu>
2017-06-30 17:13:29 -0700

    formatting

--- comparemd5.py
+++ comparemd5.py
@@ -17,13 +17,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 changed for travis
 '''
 
-
 import hashlib
 import sys
 import os
 from threading import Thread
 import argparse
-
 
 EPILOG = '''
           Usage: python comparemd5.py <dir1> <dir2>
@@ -34,6 +32,7 @@ class Md5CalculatingThread(Thread):
     '''
     Thread that compares MD5 sums
     '''
+
     def __init__(self, fname1, fname2):
         '''
         Initialize thread
@@ -57,9 +56,8 @@ class Md5CalculatingThread(Thread):
 
 def get_args():
     parser = argparse.ArgumentParser(epilog=EPILOG)
-    parser.add_argument('dirs',
-                        help='Two directories containing files to compare',
-                        nargs=2)
+    parser.add_argument(
+        'dirs', help='Two directories containing files to compare', nargs=2)
     args = parser.parse_args()
     return args
 
@@ -88,22 +86,28 @@ def main():
     print 'dir1 path: %s ' % dir_1_abspath
     print 'dir2 path: %s ' % dir_2_abspath
     # get basenames of files in both directories to find common ones
-    dir_1_basenames = {os.path.basename(fname) for
-                       fname in os.listdir(dir_1_abspath)
-                       if os.path.isfile(os.path.join(dir_1_abspath, fname))}
-    dir_2_basenames = {os.path.basename(fname) for
-                       fname in os.listdir(dir_2_abspath)
-                       if os.path.isfile(os.path.join(dir_2_abspath, fname))}
+    dir_1_basenames = {
+        os.path.basename(fname)
+        for fname in os.listdir(dir_1_abspath)
+        if os.path.isfile(os.path.join(dir_1_abspath, fname))
+    }
+    dir_2_basenames = {
+        os.path.basename(fname)
+        for fname in os.listdir(dir_2_abspath)
+        if os.path.isfile(os.path.join(dir_2_abspath, fname))
+    }
     # Make common_basenames a list to ensure order
     common_basenames = list(dir_1_basenames.intersection(dir_2_basenames))
     if not common_basenames:
         print "No common filenames were found"
         sys.exit(1)
     # Expand the basenames back into absolute paths
-    common_names_in_dir1 = [os.path.join(dir_1_abspath, name) for
-                            name in common_basenames]
-    common_names_in_dir2 = [os.path.join(dir_2_abspath, name) for
-                            name in common_basenames]
+    common_names_in_dir1 = [
+        os.path.join(dir_1_abspath, name) for name in common_basenames
+    ]
+    common_names_in_dir2 = [
+        os.path.join(dir_2_abspath, name) for name in common_basenames
+    ]
     # Do the actual comparison
     for name1, name2 in zip(common_names_in_dir1, common_names_in_dir2):
         thread = Md5CalculatingThread(name1, name2)
